bdb473bd01a36fab919c5a0ad4b8cc12af64e3ab
Hien Le <levutoanhien2703@gmail.com>
2019-05-14 15:22:54 +0700

    Add RSS_Feed

---
+++ RSS_Feed/app/src/main/java/com/example/baitap3/Category.java
@@ -0,0 +1,38 @@
+package com.example.baitap3;
+
+import java.util.List;
+
+public class Category {
+    private int ID;
+    private String CategoryName;
+    private List<Dish> Dishs;
+
+    public Category(int ID, String CategoryName, List<Dish> Dishs){
+        this.ID = ID;
+        this.CategoryName = CategoryName;
+        this.Dishs = Dishs;
+    }
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getCategoryName() {
+        return CategoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        CategoryName = categoryName;
+    }
+
+    public List<Dish> getDishs() {
+        return Dishs;
+    }
+
+    public void setDishs(List<Dish> dishs) {
+        Dishs = dishs;
+    }
+}
