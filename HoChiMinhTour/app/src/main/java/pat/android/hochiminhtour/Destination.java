7724a1170541462b462f3c9a4460e120c623b719
Truc <anhtrucphanit@gmail.com>
2019-05-14 14:41:11 +0700

    Done HoChiMinhTour

---
+++ HoChiMinhTour/app/src/main/java/pat/android/hochiminhtour/Destination.java
@@ -0,0 +1,50 @@
+package pat.android.hochiminhtour;
+
+/**
+ * Created by Trúc on 5/14/2019.
+ */
+public class Destination {
+        String name;
+        String location;
+        String webpage;
+        int image;
+
+    public Destination(String name, String location, String webpage, int image) {
+        this.name = name;
+        this.location = location;
+        this.webpage = webpage;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getWebpage() {
+        return webpage;
+    }
+
+    public void setWebpage(String webpage) {
+        this.webpage = webpage;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
---
+++ HoChiMinhTour/app/src/main/java/pat/android/hochiminhtour/DestinationAdapter.java
@@ -0,0 +1,59 @@
+package pat.android.hochiminhtour;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Trúc on 5/14/2019.
+ */
+public class DestinationAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layout;
+    private List<Destination> destinationList;
+
+    public DestinationAdapter(Context context, int layout, List<Destination> destinationList) {
+        this.context = context;
+        this.layout = layout;
+        this.destinationList = destinationList;
+    }
+
+    @Override
+    public int getCount() {
+        return destinationList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(layout,null);
+
+        // Anh xa
+        TextView txtName = (TextView) convertView.findViewById(R.id.txtname);
+        ImageView img = (ImageView) convertView.findViewById(R.id.img);
+
+        // Gan gia tri
+        Destination destination = destinationList.get(position);
+        txtName.setText(destination.getName());
+        img.setImageResource(destination.getImage());
+
+        return convertView;
+    }
+}
